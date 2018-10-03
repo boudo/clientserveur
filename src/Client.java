e0028807c9f1a3d28a876092258e358ddafd6d6c
uvsq21603700 <ibrahim.boudo@ens.uvsq.fr>
2018-10-03 16:46:37 +0200

    ajout des fichiers

---
+++ src/Client.java
@@ -0,0 +1,28 @@
+
+public class Client {
+
+	private String _nom;
+	private Serveur _serv;
+	
+	
+	public Client(String nom)
+	{
+		_nom = nom;
+	}
+	
+	public boolean seConnecter(Serveur serv)
+	{
+		return true;
+	}
+	
+	public void envoyer(String message)
+	{
+		
+	}
+	
+	public void recevoir(String message)
+	{
+		
+	}
+
+}
---
+++ src/Serveur.java
@@ -0,0 +1,20 @@
+
+public class Serveur {
+
+	arrayLyste< Client> mesClient
+	
+	public Serveur() {
+		
+	}
+	
+	public boolean connecter(Client cli)
+	{
+		return true;
+	}
+	
+	public void diffuser(String message)
+	{
+		
+	}
+
+}
